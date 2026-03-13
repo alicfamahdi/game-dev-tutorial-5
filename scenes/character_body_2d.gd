a12d74faacb4a20a267501cea051200f0ca8699a
alicfamahdi <alicfamahdi@gmail.com>
2026-03-13 15:31:51 +0700

    feat: add new animated object and hit

--- scenes/character_body_2d.gd
+++ scenes/character_body_2d.gd
@@ -1,42 +1,39 @@
 extends CharacterBody2D
 
-@export var gravity = 200.0
-@export var walk_speed = 200
-@export var jump_speed = -300
+@export var SPEED := 200
+@export var JUMP_SPEED := -400
+@export var GRAVITY := 1200
 @export var max_movement = 2
 var jump_count = 0
-var dash_count = 0
-
-var walk_texture = preload("res://assets/kenney_platformercharacters/PNG/Female/Poses/female_walk1.png")
-var jump_texture = preload("res://assets/kenney_platformercharacters/PNG/Female/Poses/female_jump.png")
-var duck_texture = preload("res://assets/kenney_platformercharacters/PNG/Female/Poses/female_duck.png")
-var idle_texture = preload("res://assets/kenney_platformercharacters/PNG/Female/Poses/female_idle.png")
 
 func _physics_process(delta):
-	velocity.y += delta * gravity
+	velocity.y += delta * GRAVITY
 
 	if is_on_floor():
 		jump_count = 0
 
 	if Input.is_action_just_pressed('ui_up') and jump_count < max_movement:
 		jump_count += 1
-		velocity.y = jump_speed
-		$Sprite2D.texture = jump_texture
+		velocity.y = JUMP_SPEED
+		$AnimatedSprite2D.play("jump")
 	
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction != 0:
 		if Input.is_action_pressed("ui_down"):
-			velocity.x = direction * (0.5 * walk_speed)
-			$Sprite2D.flip_h = direction < 0
-			$Sprite2D.texture = duck_texture
+			velocity.x = direction * (0.5 * SPEED)
+			$AnimatedSprite2D.flip_h = direction < 0
+			$AnimatedSprite2D.play("crouch")
 		else:
-			velocity.x = direction * (1 * walk_speed)
-			$Sprite2D.flip_h = direction < 0
-			$Sprite2D.texture = walk_texture
+			velocity.x = direction * (1 * SPEED)
+			$AnimatedSprite2D.flip_h = direction < 0
+			$AnimatedSprite2D.play("walk")
 	else:
 		velocity.x = 0
 		if is_on_floor():
-			$Sprite2D.texture = idle_texture
+			$AnimatedSprite2D.play("stand")
+			
+	if Input.is_action_pressed("ui_select"):
+		$AnimatedSprite2D.play("punch")
 
 	# "move_and_slide" already takes delta time into account.
 	move_and_slide()
---
+++ scenes/lava_snake.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+@export var hit_duration := 2.0
+
+var _player_in_range := false
+var _is_defeated := false
+
+@onready var _animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+	_animated_sprite.play("default")
+
+func _process(_delta: float) -> void:
+	if _is_defeated:
+		return
+
+	if _player_in_range and Input.is_action_just_pressed("ui_select"):
+		_start_defeat_sequence()
+
+func _start_defeat_sequence() -> void:
+	_is_defeated = true
+	_animated_sprite.play("hit")
+	await get_tree().create_timer(hit_duration).timeout
+	_animated_sprite.play("dead")
+
+func _on_body_entered(body: Node) -> void:
+	if body is CharacterBody2D:
+		_player_in_range = true
+
+func _on_body_exited(body: Node) -> void:
+	if body is CharacterBody2D:
+		_player_in_range = false
